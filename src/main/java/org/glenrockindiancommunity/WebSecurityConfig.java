37abb176705618e96ed4ccb4ceca11e8e372a42a
Kunal Khosla <kkhosla2@bloomberg.net>
2017-03-28 12:32:43 -0400

    Issue#8 : Adding security for admins (excluding GET). TODO: Get admin list from the repository, and remove hardcoded dummy admin

--- src/main/java/org/glenrockindiancommunity/WebSecurityConfig.java
+++ src/main/java/org/glenrockindiancommunity/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package org.glenrockindiancommunity;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -11,33 +12,31 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-  /**
-   * Stuff which should not need authentication.
-   */
-  @Override
-  public void configure(WebSecurity web) throws Exception {
-    web.ignoring().antMatchers("/static/**").antMatchers("/favicon.ico");
-  }
+    /**
+     * Stuff which should not need authentication.
+     */
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/static/**").antMatchers("/favicon.ico");
+    }
 
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
-//    http
-//      .authorizeRequests()
-//      .antMatchers(HttpMethod.POST, "/event/**")
-//      .fullyAuthenticated();
-    
-    http
-      .authorizeRequests()
-      .antMatchers(HttpMethod.DELETE, "/**")
-      .denyAll();
-  
-    http
-      .antMatcher("/**")
-      .authorizeRequests()
-      .anyRequest().permitAll();
-    
-    // For REST calls (need more investigation)
-    http.csrf().disable();
-  }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic().and().authorizeRequests()
+                .antMatchers(HttpMethod.GET,"/event/**").permitAll()
+                .antMatchers("/event/**")
+                .hasRole("ADMIN")
+                .and()
+                .antMatcher("/**")
+                .authorizeRequests()
+                .anyRequest().permitAll();
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+    }
 
+    protected void configure(AuthenticationManagerBuilder auth)
+            throws Exception {
+        auth.inMemoryAuthentication().withUser("admin1").password("secret1")
+                .roles("ADMIN");
+    }
 }
